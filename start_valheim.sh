3e97f582e8411c0b034699356d6f9cb4e7f8aa86
Cameron Wright <ccameronwright@gmail.com>
2022-02-16 23:32:11 -0700

    Container will move plugins directory to persistant storage, so user can add plugins that last.

--- start_valheim.sh
+++ start_valheim.sh
@@ -3,11 +3,19 @@
 # valheim_server.x86_64 likes to start in same directory it's located in
 cd ${STEAMAPPDIR}/valheimServer
 
+saveDir="/home/steam/.config/unity3d/IronGate/Valheim"
 # Check if Valheim Plus config file exist, will store it in persistant storage
-if [ ! -e "/home/steam/.config/unity3d/IronGate/Valheim/valheim_plus.cfg" ]; then
-    touch /home/steam/.config/unity3d/IronGate/Valheim/valheim_plus.cfg
-    ln -s /home/steam/.config/unity3d/IronGate/Valheim/valheim_plus.cfg ./BepInEx/config/
+if [ ! -e "${saveDir}/valheim_plus.cfg" ]; then
+    touch ${saveDir}/valheim_plus.cfg
+    ln -s ${saveDir}/valheim_plus.cfg ./BepInEx/config/
 fi
+
+# Check if Valheim Plus plugin directory exists, if not move to persistant storage for user
+if [ ! -d "${saveDir}/plugins" ]; then
+        cp -r ./BepInEx/plugins ${saveDir}/plugins
+        ln -s ${saveDir}/plugins ./BepInEx/config/plugins
+fi
+
 
 ./start_server_bepinex.sh \
     -name $SERVER_NAME \
@@ -21,3 +29,4 @@ trap "kill -s SIGINT $!; wait" SIGTERM
 
 # Wait for the job to end
 wait $!
+
